29f59b6d701c3033be7e024ba66bd0994f51639a
Georgi Dimov <gt_dimov@mail.bg>
2015-02-13 17:30:00 +0200

    PlayerDriver now works with Process.spawn.

--- driver.rb
+++ driver.rb
@@ -15,26 +15,34 @@ class PlayerDriver
   def start_player
     Process.wait(create_socket)
     
-    pid = fork do
-      system "mplayer -idle -input file=#{@socket_location}" 
-    end
+    pid = Process.spawn "mplayer -idle -input file=#{@socket_location}" 
 
-    Process.wait(pid)
-    delete_socket
+    pid
   end
 
   def stop_player
-    system "echo \"quit\" > /tmp/mplayer_socket"
+    system "kill #{@player_process}"
+    puts "killed #{@player_process}"
     delete_socket
   end
 
   def delete_socket
-    system " rm #{@socket_location}"
+    system "rm #{@socket_location}"
+    puts "Deleted!"
+  end
+
+  def f
+    puts "hohoh!"
   end
 end
 
 
 driver = PlayerDriver.new
+sleep(1)
+driver.f
+sleep(7)
+driver.f
+driver.stop_player
 #driver.start_player
 
 #driver.stop_player
