d2e6eacaed6cc100c22d72231e5fbe4741553192
Georgi Dimov <gt_dimov@mail.bg>
2015-02-13 21:39:55 +0200

    Add test for class Player.

---
+++ spec/player_spec.rb
@@ -0,0 +1,37 @@
+describe 'Player' do
+  let(:socket_location){ '/tmp/tmp_socket' }
+
+  let(:player){ Player.new(socket_location) }
+
+  context 'sockets' do
+    it 'create socket on the right place' do
+      player
+      expect(File.exist?(socket_location)).to eq true
+      player.stop_player
+    end
+
+    it 'remove socket after finish' do
+      player.stop_player
+      expect(File.exist?(socket_location)).to eq false
+    end
+  end
+=begin
+  let(:player_name){ 'mplayer' }
+  context 'player process' do
+    it 'run player process' do
+      player
+      grep_output = %x[ps aux | grep "#{player_name}"]
+      expect(grep_output.lines.count).to eq 2
+      player.stop_player
+    end
+
+    it 'stop player procces' do
+      player
+      player.stop_player
+      grep_output = %x[ps aux | grep player_name]
+      expect(grep_output.lines.count).to eq 1
+      puts grep_output
+    end
+  end
+=end
+end
