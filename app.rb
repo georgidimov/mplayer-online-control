c6f70e04061b9a48e07b9b561be5537d81e32c17
Georgi Dimov <gt_dimov@mail.bg>
2015-02-14 12:47:46 +0200

    Add config_file that contains location of the songs and socket.

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 require 'sinatra/json'
+require 'sinatra/config_file'
 require_relative 'lib/real_player'
 require_relative 'routes/player'
+require_relative 'lib/file_manager'
 
+config_file 'config/config.yml'
--- routes/player.rb
+++ routes/player.rb
@@ -1,27 +1,31 @@
 require 'json'
+require_relative '../lib/file_manager'
 
 get '/run' do
-  source_file = '/tmp/mplayer_socket'
-  Player.run(source_file)
-  CommandParser.run(source_file)
+  Player.run(settings.socket_location)
+  CommandParser.run(settings.socket_location)
 
   redirect '/'
 end
 
-post '/kill' do
+get '/kill' do
   Player.stop
 end
 
-get '/' do
+get '/' do 
   erb :base
+end
+
+post '/pause' do
+  CommandParser.pause
 end
 
 post '/play' do
   json = request.body.read
   song_name = JSON.parse(json)["name"]
   song_name = song_name.gsub(' ', '\ ')
-  puts song_name
-  CommandParser.play('/home/georgi/Music/last\ added', song_name)
+  path      = settings.source_folder.gsub(' ', '\ ')
+  CommandParser.play(path, song_name)
 end
 
 post '/stop' do
